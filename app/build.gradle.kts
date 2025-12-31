e151751c4749dbb75eb5694f1b6cdaddb342bdb6
Mehdikhalifeh <khalifeh.mehdi@gmail.com>
2025-12-31 00:10:59 +0000

    Fix kapt plugin alias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.kapt)
+    kotlin("kapt")
     alias(libs.plugins.kotlin.parcelize)
     alias(libs.plugins.hilt.android)
     alias(libs.plugins.kotlin.compose)
