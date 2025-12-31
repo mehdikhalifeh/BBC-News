6bfad0d27d26b69cf331bf32bb05c319ee09f1fa
Mehdikhalifeh <khalifeh.mehdi@gmail.com>
2025-12-31 12:44:51 +0000

    Use navigation-compose NavHost without transitions

--- app/src/main/java/com/mehdi/bbcnews/component/navigation/Navigation.kt
+++ app/src/main/java/com/mehdi/bbcnews/component/navigation/Navigation.kt
@@ -1,16 +1,14 @@
 package com.mehdi.bbcnews.component.navigation
 
-import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.AnimatedNavHost
+import androidx.navigation.compose.NavHost
 import com.mehdi.bbcnews.component.NewsListViewModel
 import com.mehdi.bbcnews.component.navigation.destinations.detailNewsComposable
 import com.mehdi.bbcnews.component.navigation.destinations.mainNewsComposable
 import com.mehdi.bbcnews.util.Constants.MAIN_NEWS_SCREEN
 
-@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun SetUpNavigation(
     navController: NavHostController,
@@ -19,7 +17,7 @@ fun SetUpNavigation(
     val screen = remember(navController) {
         Screens(navController = navController)
     }
-    AnimatedNavHost(navController = navController, startDestination = MAIN_NEWS_SCREEN) {
+    NavHost(navController = navController, startDestination = MAIN_NEWS_SCREEN) {
         mainNewsComposable(
             navigateToDetailNewsList = screen.main,
             newsListViewModel = newsListViewModel
--- app/src/main/java/com/mehdi/bbcnews/component/navigation/destinations/DetailNewsComposable.kt
+++ app/src/main/java/com/mehdi/bbcnews/component/navigation/destinations/DetailNewsComposable.kt
@@ -1,8 +1,5 @@
 package com.mehdi.bbcnews.component.navigation.destinations
 
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideOutHorizontally
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
@@ -12,36 +9,16 @@ import com.mehdi.bbcnews.data.util.AssetParamType
 import com.mehdi.bbcnews.util.Constants
 import com.mehdi.bbcnews.util.Constants.DETAIL_NEWS_SCREEN
 
-@OptIn(ExperimentalAnimationApi::class)
 fun NavGraphBuilder.detailNewsComposable() {
     composable(
         route = DETAIL_NEWS_SCREEN,
         arguments = listOf(navArgument(Constants.DETAIL_NEWS_ARGUMENT_KEY) {
             type = AssetParamType()
-        }),
-        enterTransition = {
-            slideInHorizontally(
-                initialOffsetX = { -it },
-                animationSpec = tween(
-                    durationMillis = 700
-                )
-            )
-        },
-        exitTransition = {
-            slideOutHorizontally(
-                targetOffsetX = { +it },
-                animationSpec = tween(
-                    durationMillis = 700
-                )
-            )
-        }
-    )
-    {
+        })
+    ) {
         val article =
             it.arguments?.getParcelable(Constants.DETAIL_NEWS_ARGUMENT_KEY, Article::class.java)
 
         article?.let { it1 -> DetailNewsList(article = it1) }
-
     }
 }
-import androidx.compose.animation.ExperimentalAnimationApi
