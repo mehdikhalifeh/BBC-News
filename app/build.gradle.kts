13857c022b8c97465987a04c8fd29bb54656e01c
mehdi khalifeh <khalifeh.mehdi@gmail.com>
2026-01-01 21:17:44 +0000

    update jvm versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,12 +44,12 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_18
+        targetCompatibility = JavaVersion.VERSION_18
     }
     kotlin {
         compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_18)
         }
     }
     buildFeatures {
